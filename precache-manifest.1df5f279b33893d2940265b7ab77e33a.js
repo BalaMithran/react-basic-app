26ab1c05a32e99f8d77aefd0eb707b893d5af296
BalaMithran <32775475+BalaMithran@users.noreply.github.com>
2020-09-18 14:18:37 +0530

    Updates

---
+++ precache-manifest.1df5f279b33893d2940265b7ab77e33a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2eb1ae606c0da937eec94c91e21587",
+    "url": "/react-basic-app/index.html"
+  },
+  {
+    "revision": "ff659f15e7935ec27811",
+    "url": "/react-basic-app/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/react-basic-app/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-app/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff659f15e7935ec27811",
+    "url": "/react-basic-app/static/js/main.96dfeda6.chunk.js"
+  },
+  {
+    "revision": "b222a8795c39491919d0",
+    "url": "/react-basic-app/static/js/runtime-main.9ac412f0.js"
+  }
+]);
---
+++ static/js/main.96dfeda6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],[,,,function(e,n,t){e.exports=t(10)},,,,,function(e,n,t){},function(e,n,t){},function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),c=t(2),o=t.n(c);t(8),t(9);var l=function(){return r.a.createElement("div",{className:"App"},"Hello World")};o.a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(l,null)),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.96dfeda6.chunk.js.map
